ff52b5683340b25f570bed202e084c137d0f079e
alejopua <alejopua@gmail.com>
2024-04-09 20:02:24 -0500

    Add new padre component and update app module and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'course-angular-s1';
+  counter: number = 0;
+
+  constructor() {}
+
+  onIncrease(): void {
+    this.counter++;
+  }
+
+  onDecrease(): void {
+    if (this.counter > 0) {
+      this.counter--;
+    }
+  }
+
+  onReset(): void {
+    this.counter = 0;
+  }
 }
